0d3fb8205fa1c8354affd199eaa09e917ae188ef
GilTorch <infotorch2014@gmail.com>
2025-01-02 11:49:31 -0400

    alg: add optimized version of bubble sort

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -13,3 +13,22 @@ function bubbleSort(arr){
 
     return arr;
 }
+
+
+function optimizedBubbleSort(arr) {
+    let fullySortedIndex = arr.length - 1;
+
+    while (fullySortedIndex > 0) {
+        let swapped = false;
+        for (let i = 0; i < fullySortedIndex; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapped = true;
+            }
+        }
+        if (!swapped) break; // Exit early if no swaps were made
+        fullySortedIndex--;
+    }
+
+    return arr;
+}
