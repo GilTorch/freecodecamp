d179e5a78b10bc1524737c3fa95507f73cb538f3
GilTorch <infotorch2014@gmail.com>
2025-01-05 18:35:04 -0400

    alg: Add remove method

--- algorithms/linkedList.js
+++ algorithms/linkedList.js
@@ -35,4 +35,60 @@ function LinkedList() {
         length = this.size() + 1;
       // Only change code above this line
     };
+
+    function LinkedList() {
+        var length = 0;
+        var head = null;
+      
+        var Node = function(element){
+          this.element = element;
+          this.next = null;
+        };
+      
+        this.size = function(){
+          return length;
+        };
+      
+        this.head = function(){
+          return head;
+        };
+      
+        this.add = function(element){
+          var node = new Node(element);
+          if(head === null){
+              head = node;
+          } else {
+            var currentNode = head;
+      
+            while(currentNode.next){
+              currentNode  = currentNode.next;
+            }
+      
+            currentNode.next = node;
+          }
+      
+          length++;
+        };
+      
+        this.remove = function(element){
+          // Only change code below this line
+          if (head.element === element) {
+            head = head.next;
+            return length--;
+          }
+          let previous = head;
+          while (previous) {
+            let current = previous.next;
+            if (current) { // makes sure we are not at end where current.next would be null
+              if (current.element === element) {
+                previous.next = current.next;
+                return length--;
+              }
+            }
+            previous = current;
+          }
+          // Only change code above this line
+        };
+      }
+    
 }
